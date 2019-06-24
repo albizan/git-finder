0b704a517fa8fe53da1881148aea2475b12b9b96
Alberto Zanotti <albizan@hotmail.it>
2019-06-24 22:23:57 +0200

    Pass onSearch to Searchbox

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import Searchbox from '../Searchbox'
 
-const Navbar = () => {
+const Navbar = ({ onSearch }) => {
   return (
     <div className="bg-white shadow-md px-4 py-2">
       <div className="font-sans container mx-auto sm:flex sm:items-center sm:justify-between">
         <div className="text-center text-2xl mb-4 sm:mb-0 ">Happy Doggy</div>
         <div className="text-center">
-          <Searchbox />
+          <Searchbox onSearch={onSearch} />
         </div>
       </div>
     </div>
