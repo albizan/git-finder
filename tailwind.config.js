556e5bd988cba835290aa8fa3f7023ecb7e91355
Alberto Zanotti <albizan@hotmail.it>
2019-06-25 21:13:43 +0200

    Changed border-radius-large to .8rems

--- tailwind.config.js
+++ tailwind.config.js
@@ -147,7 +147,7 @@ module.exports = {
       '56': '14rem',
       '64': '16rem',
     },
-    backgroundColor: theme => theme('colors'),
+    backgroundColor: (theme) => theme('colors'),
     backgroundPosition: {
       bottom: 'bottom',
       center: 'center',
@@ -164,15 +164,15 @@ module.exports = {
       cover: 'cover',
       contain: 'contain',
     },
-    borderColor: theme => ({
+    borderColor: (theme) => ({
       ...theme('colors'),
       default: theme('colors.gray.300', 'currentColor'),
     }),
     borderRadius: {
       none: '0',
-      sm: '0.125rem',
-      default: '0.25rem',
-      lg: '0.5rem',
+      sm: '0.2rem',
+      default: '0.4rem',
+      lg: '0.8rem',
       full: '9999px',
     },
     borderWidth: {
@@ -260,7 +260,7 @@ module.exports = {
       extrabold: '800',
       black: '900',
     },
-    height: theme => ({
+    height: (theme) => ({
       auto: 'auto',
       ...theme('spacing'),
       full: '100%',
@@ -357,12 +357,12 @@ module.exports = {
       '11': '11',
       '12': '12',
     },
-    padding: theme => theme('spacing'),
+    padding: (theme) => theme('spacing'),
     stroke: {
       current: 'currentColor',
     },
-    textColor: theme => theme('colors'),
-    width: theme => ({
+    textColor: (theme) => theme('colors'),
+    width: (theme) => ({
       auto: 'auto',
       ...theme('spacing'),
       '1/2': '50%',
