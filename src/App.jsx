267c512083a24a5ed20f83499ccad9b2ef895aa5
Alberto Zanotti <albizan@hotmail.it>
2019-06-25 21:16:49 +0200

    Added handleSearch, manage loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react'
+import http from './services/http'
 import Navbar from './components/Navbar'
+import Spinner from './components/Spinner'
+import UserWrapper from './components/UserWrapper'
 
 function App() {
   const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
 
-  const handleSearch = user => {
+  const handleSearch = async (user) => {
     setLoading(true)
-    console.log(user)
-    // Call Github api
+    try {
+      // Res.data.items is an array with the found users
+      const res = await http.get('', {
+        params: {
+          q: user,
+        },
+      })
+      // Extract all found users from response
+      const { items } = res.data
+      // Set new state
+      setUsers(items)
+      setLoading(false)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
     <div>
       <Navbar onSearch={handleSearch} />
+      {loading && <Spinner />}
+      {users.length !== 0 && <UserWrapper users={users} />}
     </div>
   )
 }
