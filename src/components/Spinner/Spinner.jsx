bd535c0992ad5b0227974cb301d3c8589a7eff33
Alberto Zanotti <albizan@hotmail.it>
2019-06-26 22:00:54 +0200

    Import and display a True Spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { ClapSpinner } from 'react-spinners-kit'
 
 const Spinner = () => {
   return (
-    <div>
-      <p>Loading data...</p>
+    <div className="flex justify-center items-center h-screen">
+      <ClapSpinner size={50} color="#000" />
     </div>
   )
 }
