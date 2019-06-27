f176278f6abd015820d799bec87c97703a60e1d6
Alberto Zanotti <albizan@hotmail.it>
2019-06-27 21:46:33 +0200

    Fixed Navbar Brand Name

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ onSearch }) => {
   return (
     <div className="bg-white shadow-md px-4 py-2">
       <div className="font-sans container mx-auto sm:flex sm:items-center sm:justify-between">
-        <div className="text-center text-2xl mb-4 sm:mb-0 ">Happy Doggy</div>
+        <div className="text-center text-2xl mb-4 sm:mb-0 ">Github Finder</div>
         <div className="text-center">
           <Searchbox onSearch={onSearch} />
         </div>
