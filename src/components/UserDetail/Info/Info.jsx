1fcdb4004d0fa2d5b3e9b7eece777fa9385fc1a1
Alberto Zanotti <albizan@hotmail.it>
2019-06-27 21:36:04 +0200

    Remove Bio header

--- src/components/UserDetail/Info/Info.jsx
+++ src/components/UserDetail/Info/Info.jsx
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react'
 
-const Info = () => {
+const Info = ({ email, company, website, location, login }) => {
   return (
     <Fragment>
-      <div></div>
+      <div className="mt-6 py-3 text-left border-t border-gray-400 md:border-0">
+        {login && <p className="">{`Username: ${login}`}</p>}
+        {email && <p className="">{`Email: ${email}`}</p>}
+        {website && <p className="">{`Website: ${website}`}</p>}
+        {company && <p className="">{`Company: ${company}`}</p>}
+        {location && <p className="">{`Location: ${location}`}</p>}
+      </div>
     </Fragment>
   )
 }
