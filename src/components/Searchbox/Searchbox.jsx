28719ead87953983ac7ef197825aa59175b3799a
Alberto Zanotti <albizan@hotmail.it>
2019-06-28 12:11:33 +0200

    Input and button are now linked to a form

--- src/components/Searchbox/Searchbox.jsx
+++ src/components/Searchbox/Searchbox.jsx
@@ -4,36 +4,36 @@ const Searchbox = ({ onSearch }) => {
   const [user, setUser] = useState('')
   const [disabled, setDisabled] = useState(true)
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     if (user) {
       onSearch(user)
-      return
     }
   }
 
   return (
-    <div className="flex items-center justify-center">
+    <form onSubmit={handleSubmit} className="flex items-center justify-center text-xs sm:text-sm">
       <input
         value={user}
-        onChange={e => {
+        onChange={(e) => {
           setUser(e.target.value)
           const isDisabled = e.target.value === '' ? true : false
           setDisabled(isDisabled)
         }}
         placeholder="Search Github User..."
-        className="outline-none focus:shadow-outline focus:bg-white bg-gray-100 rounded-lg px-6 py-2"
-        type="text"
+        className="focus:shadow-outline focus:bg-white bg-gray-200 rounded-lg px-4 py-2"
+        type="search"
       />
       <button
-        type="button"
-        disabled={disabled && true}
-        onClick={handleSubmit}
-        className={`rounded-full bg-blue-500 hover:bg-blue-700 ouline-none text-white font-semibold px-5 py-2 ml-4 ${disabled &&
+        type="submit"
+        disabled={disabled}
+        onKey
+        className={`rounded-full bg-blue-500 hover:bg-blue-700 text-white font-semibold px-4 py-2 ml-4 ${disabled &&
           'opacity-50 cursor-not-allowed'}`}
       >
         Search
       </button>
-    </div>
+    </form>
   )
 }
 
