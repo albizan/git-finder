ebeb0dc59587c8f64debb031cb5c79f83b6e00e0
Alberto Zanotti <albizan@hotmail.it>
2019-06-28 18:48:02 +0200

    Set Error message when an error occured

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 import Spinner from '../../components/Spinner'
+import UserNotFound from '../../components/UserNotFound'
 import UserDetail from '../../components/UserDetail'
 import http from '../../services/http'
 import { Link } from 'react-router-dom'
@@ -8,25 +10,31 @@ const User = ({ match }) => {
   const { login } = match.params
 
   const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState({})
+  const [error, setError] = useState(false)
+  const [user, setUser] = useState(null)
+
+  async function getUserDetailsFromGithub() {
+    setLoading(true)
+    try {
+      const { data } = await http.get(`/users/${login}`)
+      console.log(data)
+      setLoading(false)
+      setUser(data)
+    } catch (err) {
+      setLoading(false)
+      setError(true)
+    }
+  }
 
   useEffect(() => {
-    async function getUserDetailsFromGithub() {
-      const { data } = await http.get(`/users/${login}`)
-      setUser(data)
-    }
-    try {
-      setLoading(true)
-      getUserDetailsFromGithub()
-      setLoading(false)
-    } catch (err) {
-      console.log(err)
-    }
-  }, [login])
+    getUserDetailsFromGithub()
+  }, [])
 
   return (
     <div className="py-4 flex flex-col items-center justify-center">
-      {loading ? <Spinner /> : <UserDetail user={user} />}
+      {loading && <Spinner />}
+      {error && <UserNotFound login={login} />}
+      {user && <UserDetail user={user} />}
       <Link
         to="/"
         className="w-auto shadow px-4 py-3 mt-4 font-bold border-blue-600 border-2 text-blue-600 bg-white rounded hover:bg-blue-600 hover:text-white"
