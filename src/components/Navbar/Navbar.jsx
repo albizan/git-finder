b4caf32fab19ec76d29f65e4f90ca16df22ced40
Alberto Zanotti <albizan@hotmail.it>
2019-06-28 12:11:01 +0200

    Decreased font size on mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import Searchbox from '../Searchbox'
 
 const Navbar = ({ onSearch }) => {
   return (
-    <div className="bg-white shadow-md px-4 py-2">
-      <div className="font-sans container mx-auto sm:flex sm:items-center sm:justify-between">
-        <div className="text-center text-2xl mb-4 sm:mb-0 ">Github Finder</div>
-        <div className="text-center">
+    <div className="bg-white shadow-md py-2">
+      <div className="font-sans sm:flex sm:items-center sm:justify-around">
+        <div className="text-center text-3xl text-gray-700 font-semibold  mb-2 sm:mb-0 ">
+          Github Finder
+        </div>
+        <div className="">
           <Searchbox onSearch={onSearch} />
         </div>
       </div>
