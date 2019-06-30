564cd41feac3dbe20d656aac0173d3ed49041bcc
Alberto Zanotti <albizan@hotmail.it>
2019-06-30 19:07:32 +0200

    Badges are now on two lines

--- src/components/UserDetail/Badges/Badges.jsx
+++ src/components/UserDetail/Badges/Badges.jsx
@@ -3,18 +3,22 @@ import React, { Fragment } from 'react'
 const Badges = ({ followers, following, gists, repos }) => {
   return (
     <Fragment>
-      <div className="w-full flex justify-center items-center mb-3 py-3 text-xs border-t border-gray-400">
+      <div className="w-full flex justify-center items-center mb-3 py-3 text-xs border-t border-gray-300">
         <div className="w-24 text-center px-1 py-2 rounded-full mx-1 bg-green-500 text-white font-semibold">
-          {followers + ' followers'}
+          <div className="text-sm">{followers}</div>
+          <div>Followers</div>
         </div>
         <div className="w-24 text-center px-1 py-2 rounded-full mx-1 bg-orange-500 text-white font-semibold">
-          {following + ' following'}
+          <div className="text-sm">{following}</div>
+          <div>Following</div>
         </div>
         <div className="w-24 text-center px-1 py-2 rounded-full mx-1 bg-red-500 text-white font-semibold">
-          {gists + ' gists'}
+          <div className="text-sm">{gists}</div>
+          <div>Gists</div>
         </div>
         <div className="w-24 text-center px-1 py-2 rounded-full mx-1 bg-indigo-500 text-white font-semibold">
-          {repos + ' repos'}
+          <div className="text-sm">{repos}</div>
+          <div>Repos</div>
         </div>
       </div>
     </Fragment>
