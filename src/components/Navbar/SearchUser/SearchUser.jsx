dc31578fbdb0d65f48f537d87b20fef8aa6b7609
Alberto Zanotti <albizan@hotmail.it>
2019-07-12 20:32:23 +0200

    Bigger font

--- src/components/Navbar/SearchUser/SearchUser.jsx
+++ src/components/Navbar/SearchUser/SearchUser.jsx
@@ -25,13 +25,13 @@ const Searchbox = ({ onSearch }) => {
           setDisabled(isDisabled)
         }}
         placeholder="Enter a user..."
-        className="outline-none rounded border-indigo-500 bg-indigo-500 text-gray-300 px-4 py-2"
+        className="outline-none rounded border-indigo-500 bg-indigo-500 text-lg text-gray-200 px-4 py-2"
         type="search"
       />
       <button
         type="submit"
         disabled={disabled}
-        className={`outline-none uppercase rounded border border-gray-300 font-semibold  text-gray-300 hover:bg-gray-300 hover:text-indigo-500 px-4 py-2 ml-4 ${disabled &&
+        className={`outline-none uppercase rounded border border-gray-300 font-semibold  text-gray-200 hover:bg-gray-300 hover:text-indigo-500 px-4 py-2 ml-4 ${disabled &&
           'opacity-50 cursor-not-allowed'}`}
       >
         Search
