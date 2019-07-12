b1603b1a18583e9c4b6fe76bd31babea6c022fef
Alberto Zanotti <albizan@hotmail.it>
2019-07-12 15:26:31 +0200

    Changed text-color

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,10 +6,10 @@ const Landing = () => {
   return (
     <Fragment>
       <div className="landing h-screen w-full flex flex-col items-center justify-center">
-        <div className="text-center text-white">
+        <div className="text-center text-gray-200">
           <p className="text-6xl">Git Finder</p>
         </div>
-        <div className="text-gray-300 text-lg">
+        <div className="text-gray-300 font-thin text-xl">
           <p>Checkout repos, gists and followers</p>
         </div>
         <SearchUser />
