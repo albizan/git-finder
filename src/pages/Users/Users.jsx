a7c0ce10887952bf7982b6ce713c1c86b4fb4d62
Alberto Zanotti <albizan@hotmail.it>
2019-07-12 20:13:51 +0200

    Created Users Page

---
+++ src/pages/Users/Users.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment, useContext } from 'react'
+
+// Import default Components
+import Navbar from '../../components/Navbar'
+
+const Users = () => {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  )
+}
+
+export default Users
