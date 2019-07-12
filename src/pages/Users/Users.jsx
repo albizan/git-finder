62dd3afb233ae9f7aebb45fa69e34317eb873762
Alberto Zanotti <albizan@hotmail.it>
2019-07-12 20:32:49 +0200

    Import UserWrapper component

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -2,11 +2,13 @@ import React, { Fragment, useContext } from 'react'
 
 // Import default Components
 import Navbar from '../../components/Navbar'
+import UserWrapper from '../../components/UserWrapper'
 
 const Users = () => {
   return (
     <Fragment>
       <Navbar />
+      <UserWrapper />
     </Fragment>
   )
 }
